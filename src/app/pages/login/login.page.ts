fb5b36d05c7d5a758d8ca55439a7e8d634f7b3b8
Monica <monibrave1@gmail.com>
2025-05-11 11:07:32 -0300

    modificacion login.pate.ts y .html para mostrar errores en validacion, tambin se agrego estilo de error

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-import { ToastController } from '@ionic/angular';
+import { ToastController, LoadingController } from '@ionic/angular';
+import { ValidationUtils } from 'src/app/utils/validation';
 
 
 @Component({
@@ -13,11 +14,15 @@ export class LoginPage {
 
   email = '';
   password = '';
+  emailError = '';//captura error de email
+  passwordError = '';//captura error de contraseña
   isLoggedIn = false; // logueado por defecto false
+  loading : HTMLIonLoadingElement | null = null ;
 
   constructor(
     private authService: AuthService, 
-    private toastCtrl: ToastController
+    private toastCtrl: ToastController,
+    private loadingCtrl: LoadingController
   ) {}
   
   /**
@@ -26,15 +31,41 @@ export class LoginPage {
    * @memberof LoginPage
    */
   async login() {
-    try {
-      await this.authService.login(this.email, this.password);
-      //solo se ejecutaran estas lineas si await se ejecuta exitosamente
-      this.isLoggedIn = true;
-      this.showToast('Login exitoso');
-    } catch (error:any) {
-      this.showToast('Error: ' + error.message);
+    //vaciamos variables de errores anteriores
+    this.emailError = '';
+    this.passwordError = '';
+    
+    //valida campos antes de hacer envio de credenciales a firebase
+    if (this.isValidForm()) {
+      //Mostrar loading antes de llamar a Firebase
+      this.loading = await this.loadingCtrl.create({
+        message: 'Iniciando sesión...',
+      });
+
+      await this.loading.present();
+
+      try {
+
+        await this.authService.login(this.email, this.password);
+        //solo se ejecutaran estas lineas si await se ejecuta exitosamente
+        //siguiente paso capturar el usuario logueado para persistir la sesion
+        //dar posibilidad de logout
+        this.isLoggedIn = true;
+        this.showToast('Login exitoso');
+
+      } catch (error: any) {
+
+        this.showToast('Error: ' + error.message);
+
+      }finally{
+
+        //Ocultar loading después de completar el login
+        await this.loading.dismiss();
+
+      }
     }
   }
+
   /**
    *Muestra temporalmente el mensaje ingresado por parametro
    *
@@ -48,6 +79,43 @@ export class LoginPage {
       position: 'top'
     });
     toast.present();
+  }
+
+  isValidForm():boolean{
+
+    //variable interna para detectar errores
+    let isValid = true;
+
+    //Validaciones antes de enviar credenciales
+    //email
+    if (ValidationUtils.isFieldEmpty(this.email)) {
+
+      this.emailError = 'El email es requerido.';
+
+      isValid = false;
+
+    } else if (!ValidationUtils.isValidEmail(this.email)) {
+
+      this.emailError = 'El email no es válido.';
+
+      isValid = false;
+    }
+
+    //contraseña
+    if (ValidationUtils.isFieldEmpty(this.password)) {
+
+      this.passwordError = 'La contraseña es requerida.';
+
+      isValid = false;
+
+    } else if (!ValidationUtils.isPasswordValid(this.password)) {
+
+      this.passwordError = 'La contraseña debe tener al menos 6 caracteres.';
+      isValid = false;
+
+    }
+
+    return isValid;
   }
 
 }
