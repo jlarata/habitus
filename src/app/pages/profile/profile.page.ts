54370c248b079d3268204ba2e0fa13b85b61dca6
j <ariel@cfee.gob.ar>
2025-06-03 11:52:24 -0300

    reescritos los métodos de profile para mostrar al usuario. creado método de put para modificar datos de perfil. recibida la info del usuario desde el componente recetas y armado un método para recuperar recetas favoritas que ya funciona (horrible) (#42)

--- src/app/pages/profile/profile.page.ts
+++ src/app/pages/profile/profile.page.ts
@@ -4,6 +4,7 @@ import { User } from 'src/app/models/user.model';
 import { AuthService } from 'src/app/services/auth.service';
 import { UsersService } from 'src/app/services/users.service';
 import { ValidationUtils } from 'src/app/utils/validation';
+import { EventDay } from "./calendar.model";
 
 
 @Component({
@@ -13,23 +14,26 @@ import { ValidationUtils } from 'src/app/utils/validation';
   standalone: false
 })
 export class ProfilePage implements OnInit {
- 
-  userData:User = {
-    uid: '',
-    email: '',
-    name: '',
-    lastName: '',
-    dateBirth: '',
-    biologicalSex:'',
-    weight: 0,
-    heigth: 0,
-    age: 0,
-    //levelBaja: false,
-    //levelMedia: false,
-    //levelAlta: false,
-    levelActivity: ''
-  };
-  
+
+  usuario: User = {
+    UID: '',
+    nombre: '',
+    apellido: '',
+    calendar_event: {},
+    celiaco: false,
+    fecha_de_nacimiento: '',
+    sexo: '',
+    mail: '',
+    vegano: false,
+    vegetariano: false,
+    peso: 0,
+    altura: 0,
+    edad: 0,
+    hace_actividad_fisica_regular: '',
+    recetas_favoritas: [],
+    events_array: EventDay[]
+  }
+
   loading: HTMLIonLoadingElement | null = null;
 
   dateError:string= "";
@@ -47,23 +51,17 @@ export class ProfilePage implements OnInit {
     private toastCtrl: ToastController,
     private loadingCtrl: LoadingController,
     private auth:AuthService,
-    private userService:UsersService
+    private userService:UsersService,
   ) 
-  { 
-    this.loadUserProfile();
+  {}
+
+  async ngOnInit() {
+   this.usuario = await this.loadUserProfile();
+   console.log("Datos del usuario cargados:", this.usuario);
   }
 
-  ngOnInit() {
-    
-  }
 
-  //- metodo guardado en firestore
-  /**
-   *metodo que actualiza en firestore parte del perfil del usuario
-   *
-   * @memberof ProfilePage
-  */
-  async saveProfile() {
+  async guardarPerfil() {
 
     // validar fecha y calcular edad
     this.validateDate();
@@ -80,52 +78,41 @@ export class ProfilePage implements OnInit {
 
     await this.loading.present();
 
-    let user:User = { 
-      email: this.userData.email,
-      uid: this.userData.uid,
-      name: this.userData?.name,
-      lastName:this.userData?.lastName,
-      weight: this.userData?.weight, 
-      heigth: this.userData?.heigth, 
-      biologicalSex: this.userData?.biologicalSex || 'X',
-      levelActivity: this.userData?.levelActivity,
-      dateBirth: this.userData?.dateBirth,
-      age: this.userData.age
-    };
-
-    try {
-      await this.userService.actualizarPerfilUsuario(this.userData.email, user);
-
-      this.showToast("Perfil actualizado correctamente.");
-
-    } catch (error) {
-
-      this.showToast("Error al actualizar perfil: " + error);
-
-      console.error("Error en perfil:", error);
-
+    const nueva_data_de_usuario = {
+      nombre: this.usuario?.nombre,
+      apellido:this.usuario?.apellido,
+      peso: this.usuario?.peso, 
+      altura: this.usuario?.altura, 
+      sexo: this.usuario?.sexo || '',
+      hace_actividad_fisica_regular: this.usuario?.hace_actividad_fisica_regular,
     }
 
+    console.log('Perfil guardado:', 
+    nueva_data_de_usuario);
+
+    this.userService.actualizarUsuario(this.usuario.mail!, nueva_data_de_usuario)
     console.log("datos guardados!: " + user);
+    this.showToast('Datos guardados!');
     
     //Ocultar loading después de completar el ""guardado""
     await this.loading.dismiss();
   }
  
   validateDate(){
-    if (!this.userData?.dateBirth) {
+
+    if (!this.usuario?.fecha_de_nacimiento) {
       //no lanzo error por que no es obligatoria la fecha
-      this.userData.dateBirth = "";
+      this.usuario.fecha_de_nacimiento = "";
       return;
     }
 
-    if (!ValidationUtils.isValidDate(this.userData.dateBirth)) {
+    if (!ValidationUtils.isValidDate(this.usuario.fecha_de_nacimiento)) {
       this.dateError = "Formato o fecha inválida. Ejemplo: 15/02/1998";
       return;
     }
 
     //si esta todo ok calculamos edad
-    this.userData.age = ValidationUtils.calculateAge(this.userData.dateBirth);
+    this.usuario.edad = ValidationUtils.calculateAge(this.usuario.nueva_data_de_usuario);
 
     this.dateError = "";
 
@@ -133,12 +120,12 @@ export class ProfilePage implements OnInit {
 
   //elimine validaciones apellido para que sea opcional
   validateNombre() {
-    if (!this.userData?.name) {
+    if (!this.usuario?.nombre) {
       this.nombreError = "Por favor complete el campo";
       return;
     }
 
-    if (this.userData.name.length < 3) {
+    if (this.usuario.nombre.length < 3) {
       this.nombreError = "Campo Vacío.";
       return;
     }
@@ -161,12 +148,6 @@ export class ProfilePage implements OnInit {
     toast.present();
   }
 
- 
-
-  //falta:
-  ///-metodo calcular edad desde fecha de nacimiento( en utils?)//falta campo edad readonly
- 
-    
   /**
   *recupera apartir del email los datos de usuario
   *almacenados en firestore
@@ -174,50 +155,43 @@ export class ProfilePage implements OnInit {
   * @return {*} 
   * @memberof ProfilePage
   */
-  async loadUserProfile() {
+  async loadUserProfile():Promise<User> {
     try {
       // Obtener el usuario autenticado desde Firebase
       const userFirebase = this.auth.getCurrentUser();
+      //obtener los datos de firestore usando el usuario de firebase
+      const user = await this.userService.obtenerPerfilUsuario(userFirebase!.email!);
 
-      if (!userFirebase?.email) {
-        this.showToast("No hay usuario autenticado o no tiene un email válido.");
-        console.error("No hay usuario autenticado o no tiene un email válido.");
-        return;
-      }
-
-      // Obtener los datos desde Firestore directamente
-      const user = await this.userService.obtenerPerfilUsuario(userFirebase.email);
-
-      if (!user) {
-        console.error("No se encontró perfil de usuario en Firestore.");
-        this.showToast("No se encontró perfil de usuario en Firestore.");
-        return;
-      }
-
-      // Asignar los datos obtenidos al objeto userData
-      this.userData = {
-        uid: user.uid || "",
-        email: user.email || "",
-        name: user.name ||"",
-        lastName: user.lastName || "",
-        dateBirth: user.dateBirth || "", 
-        biologicalSex:user.biologicalSex || '',
-        weight: user.weight || 0,   // Peso en kg (opcional)
-        heigth: user.heigth || 0,  // Altura en cm (opcional)
-        age: user.age || 0, //calculado desde la fecha de nacimiento//aun no implementado
-        //levelBaja: true,
-        //levelMedia: false,
-       // levelAlta: false,
-        levelActivity: user.levelActivity || "Baja"
+      //construyo un profile con los datos y lo devuelvo
+      // btw creo que este paso es al pedo. podiamos retornar el user
+      
+      let currentProfile = {
+        UID: user?.UID || "",
+        mail: user?.mail || "",
+        vegetariano: user?.vegetariano,
+        nombre: user?.nombre ||"",
+        apellido: user?.apellido || "",
+        fecha_de_nacimiento: user?.fecha_de_nacimiento || undefined, 
+        sexo:user?.sexo || '',
+        celiaco: user?.celiaco,
+        vegano: user?.vegano,
+        peso: user?.peso || 0,
+        //weight: user?.weight || 0,   // Peso en kg (opcional)
+        altura: user?.altura || 0,  // Altura en cm (opcional)
+        edad: user?.edad || 0, //calculado desde la fecha de nacimiento//aun no implementado
+        hace_actividad_fisica_regular: user?.hace_actividad_fisica_regular,
+        recetas_favoritas: user?.recetas_favoritas,
+        calendar_event: user?.calendar_event
       };
 
-      console.log("Datos del usuario cargados:", this.userData);
+      console.log('usuario cargado en el método: ',currentProfile)
+      return currentProfile!
 
     } catch (error) {
-
       console.error("Error al obtener los datos del usuario:", error);
-
       this.showToast("Error al obtener los datos del usuario: " + error);
+      
+      return this.usuario
     }
   }
 
--- src/app/services/users.service.ts
+++ src/app/services/users.service.ts
@@ -17,61 +17,46 @@ export class UsersService {
 
     constructor() { }
 
-    public async obtenerUsuarios() {
-
-        /* console.log(getDocs(collection(db, "users"))) */
-        const querySnapshot = await getDocs(collection(db, "users"));
-        console.log(querySnapshot.docs[1].data())
-        console.log('mail id: ' + querySnapshot.docs[1].data()['mail'])
-        console.log('recetas favoritas: ' + querySnapshot.docs[1].data()['recetas_favoritas'])
-        console.log('es veganx?: ' + querySnapshot.docs[1].data()['vegano'])
-        console.log('es celiacx?: ' + querySnapshot.docs[1].data()['celiaco'])
-        console.log('se ejercita?: ' + querySnapshot.docs[1].data()[' hace_actividad_fisica_regular'])
-        console.log('peso: ' + querySnapshot.docs[1].data()['peso'] + 'Kgs')
-        console.log('eventos agendados?: ' + querySnapshot.docs[1].data()['calendar_event']['title'])
-        /* querySnapshot.forEach((doc) => {
-            console.log(`${doc.id} => ${doc.data()}`);
-        }); */
-    }
-
     /**
      *
      *Al crear usuario inicializamos una collecion en firestore 
      *con datos preinicializados del usuario. 
-     *usamos eail como id de la colleccion
-     * @param {string} email
-     * @param {*} uid
+     *usamos email como id de la colleccion
+     * @param {string} mail
+     * @param {*} uid //POR AHORA NO LO USAMOS
      * @memberof UsersService
      */
-    public async crearDataUsuario(email: string, uid: any) {
+    public async crearDataUsuario(mail:string//, uid:any POR AHORA SACO ESTO SI NO LO USAMOS
+        ) {
         try {
-            const nuevoUsuario: User = {
-                uid: uid, // voy a almacenar el id uduario por las dudas
-                image: "",
-                email: email,
-                name: "",
-                lastName: "",
-                dateBirth: "", //dd/mm/yy
-                biologicalSex: "X",
-                weight: 0,   // Peso en kg (opcional)
-                heigth: 0,  // Altura en cm (opcional)
-                age: 0, //calculado desde la fecha de nacimiento
-                levelActivity: "Media", //Baja, Media, Alta
+            const nuevoUsuario:User = {
+                UID: "", // AQUÍ HABRÍA QUE HACER UN MÉTODO EN DOS PASOS. PRIMERO AWAIT CREAR USER 
+                // EN FIREBASE AUTTH, LUEGO GET ESOS DATOS INCLUYENDO MAIL Y UID Y USAR ESO PARA LA BBDD EN FIRESTORE
+                // voy a almacenar el id uduario por las dudas
+                //image:"",
+                mail: mail,
+                nombre: "",  
+                apellido: "", 
+                //dateBirth: "", //dd/mm/yy
+                sexo: "X",    
+                peso: 0, // Peso en kg (opcional)
+                altura: 0,  // Altura en cm (opcional)
+                edad:0, //calculado desde la fecha de nacimiento
+                hace_actividad_fisica_regular:"medio", //Baja, Media, Alta
                 calendar_event: [], //esto tiene q ser un array de 
                 //eventuales objetos
                 celiaco: false,
                 recetas_favoritas: [],
                 vegano: false,
                 vegetariano: false,
-
                 events_array: [],
             };
 
             //pone id random a la colleccion
             //const docRef = await addDoc(collection(db, "users"),nuevoUsuario);
 
-            //enviamos el email para id de coleccion
-            const docRef = await setDoc(doc(db, "users", email), nuevoUsuario);
+            //enviamos el email para id de coleccion <- qué era esto? ya no sirve?
+            const docRef = await setDoc(doc(db, "users", mail), nuevoUsuario);
 
             console.log("Usuario agregado exitosamente. " + docRef);
 
@@ -85,23 +70,28 @@ export class UsersService {
     /**
      *busca coleccion por id (email) que se le pasa por parametro
      *retorna promesa datos en forma de la interface User o null si da error
-     * @param {string} email
+     * @param {string} mail
      * @return {*}  {(Promise<User | null>)}
      * @memberof UsersService
     */
-    public async obtenerPerfilUsuario(email: string): Promise<User | null> {
+    public async obtenerPerfilUsuario(mail: string): Promise<User | null> {
         //voy a atrapar los errores desde profile.page.ts
-        const docRef = doc(db, "users", email); // referencia al doc por email
+        const docRef = doc(db, "users", mail); // referencia al doc por email
         const docSnap = await getDoc(docRef); // obtiene el documento desde Firestore
 
         if (!docSnap.exists()) {
-            throw new Error(`No se encontró perfil de usuario con email: ${email}`);
+            throw new Error(`No se encontró perfil de usuario con email: ${mail}`);
         }
 
+        const perfil = docSnap.data() as User;
+        //console.log("el servicio devuelve: ", perfil)
         return docSnap.data() as User;
     }
     
     /**
+     * Ariel: SI NO ME EQUIVOCO este método queda obsoleto por el otro que armé más abajo. Lo comento por ahora
+     * si luego nada se rompe vemos de borrarlo.
+     *
      *updateDoc() → Actualiza solo los campos enviados (método de firestore)
      *Partial<T> es un tipo genérico en TypeScript
      *Partial<User> → Permite que solo pases los datos que quieres modificar.
@@ -112,22 +102,15 @@ export class UsersService {
      * @return {*}  {Promise<void>}
      * @memberof UsersService
     */
-    public async actualizarPerfilUsuario(email: string, datosActualizados: Partial<User>): Promise<void> {
+    /*public async actualizarPerfilUsuario(email: string, datosActualizados: Partial<User>): Promise<void> {
         const docRef = doc(db, "users", email); 
         await updateDoc(docRef, datosActualizados);
-    }
+    }*/
 
-    /*    async getEventsArray(email: string) : Promise<EventDay[]>{
-           const docRef = doc(db, "users", email); // referencia al doc por email
-           const docSnap = await getDoc(docRef); // obtiene el documento desde Firestore
-   
-           if (!docSnap.exists()) {
-               throw new Error(`No se encontró perfil de usuario con email: ${email}`);
-           }
-   
-           return docSnap.data() as User;
-       }
-    */
+    /*este es el método para updatear desde el calendario no? documentémoslo si es así
+    no se por qué hablo en plural si lo hice yo, debería comentarlo yo
+    bueno estoy inseguro*/
+  
     async saveEventsArray(email: string, events_array: EventDay[]) {
 
         const docRef = doc(db, "users", email); // referencia al doc por email
@@ -139,5 +122,33 @@ export class UsersService {
 
         //console.log(events_array, " updateado para ", email);
     }
+
+    async actualizarUsuario(mail: string, nueva_data_de_usuario:any) {
+        const docRef = doc(db, "users", mail); // referencia al doc por email
+        await updateDoc(docRef, nueva_data_de_usuario)
+    } 
+
+
+
+
+
+    //funcion para probar cositas, probablemente la eliminaremos de prod
+    
+    /* public async obtenerUsuarios() {
+
+        // console.log(getDocs(collection(db, "users")))
+        const querySnapshot = await getDocs(collection(db, "users"));
+        console.log(querySnapshot.docs[1].data())
+        console.log('mail id: '+querySnapshot.docs[1].data()['mail'])
+        console.log('recetas favoritas: '+querySnapshot.docs[1].data()['recetas_favoritas'])
+        console.log('es veganx?: '+ querySnapshot.docs[1].data()['vegano'])
+        console.log('es celiacx?: '+querySnapshot.docs[1].data()['celiaco'])
+        console.log('se ejercita?: '+querySnapshot.docs[1].data()['hace_actividad_fisica_regular'])
+        console.log('peso: '+querySnapshot.docs[1].data()['peso']+'Kgs')
+        console.log('eventos agendados?: '+querySnapshot.docs[1].data()['calendar_event']['title'])
+        // querySnapshot.forEach((doc) => {
+          //  console.log(`${doc.id} => ${doc.data()}`);
+        //}); 
+    } */
 
 }
