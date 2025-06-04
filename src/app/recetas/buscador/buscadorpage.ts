bb683bda006a017baa8e4f488327bc55d4fda0ff
j <ariel@cfee.gob.ar>
2025-06-04 13:16:25 -0300

    agregada logica de mostrar recetas favoritas, un poco de refactorizacion para hacer más simple el buscador (#52)

--- src/app/recetas/buscador/buscadorpage.ts
+++ src/app/recetas/buscador/buscadorpage.ts
@@ -1,7 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { SpoonacularService } from '../../services/spoonacular.service';
 import { QueryDeRecetas } from '../../models/recetas';
-
+import { CalculationUtils } from 'src/app/utils/calculations';
 
 @Component({
   selector: 'app-buscador',
@@ -13,16 +13,16 @@ export class BuscadorPage {
 
   @Input() busquedaDesplegada?: boolean;
   @Input() queryDeRecetas?: QueryDeRecetas;
+  @Input() recetasFavoritas?: string[];
 
   public vegetarianIsChecked = false;
   public glutenFreeIsChecked = false;
   public veganIsChecked = false;
-  
+
   //busquedaDesplegada = false;
 
 
   totalResults?: number;
-  recetasPerPage = 10;
   recetasPagesNumber?: number;
   currentPage = 1;
 
@@ -37,7 +37,8 @@ export class BuscadorPage {
     this.buscarRecetas();
   }
 
-  constructor(public spoonacular: SpoonacularService) { }
+  constructor(public spoonacular: SpoonacularService,
+  ) { }
   ngOnInit() {
     //le saco lo automático al método / this.ionViewDidLoad()
   }
@@ -50,34 +51,51 @@ export class BuscadorPage {
         (data) => {
           //console.log(data),
           this.queryDeRecetas = data,
-          this.calcularPaginas(this.queryDeRecetas.totalResults!)
+          this.recetasPagesNumber = CalculationUtils.calcularPaginas(this.queryDeRecetas.totalResults!)
           this.busquedaDesplegada = !this.busquedaDesplegada
 
-            //para testear console.log(this.queryDeRecetas.results),
-            this.queryDeRecetasChange.emit(this.queryDeRecetas)
+          //para testear console.log(this.queryDeRecetas.results),
+          this.queryDeRecetasChange.emit(this.queryDeRecetas)
         },
         (error) => { console.log(error); }
       )
   }
 
-   /**
-   * @function blanquearRecetas limpia la query, el DOM deja de mostrar las cards.
-   */
-   public blanquearRecetas() {
+  /**
+  * @function blanquearRecetas limpia la query, el DOM deja de mostrar las cards.
+  */
+  public blanquearRecetas() {
     this.queryDeRecetas = undefined;
     this.busquedaDesplegada = !this.busquedaDesplegada;
     this.busquedaDesplegadaChange.emit(!this.busquedaDesplegada)
     this.queryDeRecetasChange.emit(this.queryDeRecetas)
-    }
-
-  calcularPaginas = (totalResults:number) => {
-    this.totalResults = totalResults;
-    this.recetasPagesNumber =  Math.ceil(this.totalResults! / this.recetasPerPage);
-
-    console.log(`se requieren ${this.recetasPagesNumber} páginas`);
   }
 
   
+
+  buscaFavoritas = (recetasFavoritas: string[]) => {
+    //debug: console.log("buscando las siguientes recetas: ", recetasFavoritas)
+
+    this.spoonacular.obtenerVariasRecetasPorID(recetasFavoritas!)
+      .subscribe(
+        (data: any) => {
+          //debug: console.log(data)
+          if (this.queryDeRecetas == undefined) {
+            this.queryDeRecetas = new QueryDeRecetas;
+          }
+          
+          this.queryDeRecetas.number = recetasFavoritas.length,
+          this.queryDeRecetas!.offset = 0,
+          this.queryDeRecetas!.results = data,
+          this.queryDeRecetas!.totalResults = recetasFavoritas.length
+
+          this.recetasPagesNumber = CalculationUtils.calcularPaginas(this.queryDeRecetas!.totalResults!)
+          this.busquedaDesplegada = !this.busquedaDesplegada
+          this.queryDeRecetasChange.emit(this.queryDeRecetas)
+        }
+      )
+    //debug: console.log(this.queryDeRecetas) 
+  }
 }
 
 
